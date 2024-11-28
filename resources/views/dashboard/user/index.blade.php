e5023f69cd919b1076443ddd731547a64428d02b
joedvn <joedemarrosero.1724@gmail.com>
2024-11-28 15:31:26 +0800

    role system update index users

--- resources/views/dashboard/user/index.blade.php
+++ resources/views/dashboard/user/index.blade.php
@@ -24,7 +24,6 @@
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-
                     <th scope="col" class="px-6 py-3">Id</th>
                     <th scope="col" class="px-6 py-3">Name</th>
                     <th scope="col" class="px-6 py-3">Email</th>
@@ -35,12 +34,19 @@
             <tbody>
                 @foreach ($users as $user)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-
                         <td class="px-6 py-4">{{ $user->id }}</td>
                         <td class="px-6 py-4">{{ $user->name }}</td>
                         <td class="px-6 py-4">{{ $user->email }}</td>
                         <td class="px-6 py-4">
-                            {{ $user->role == App\Models\Role::ADMIN ? 'Admin' : 'User' }}
+                            {{-- Display role based on ID --}}
+                            @if ($user->role == 1)
+                                User
+                            @elseif ($user->role == 2)
+                                Admin
+                            @elseif($user->role==3)
+                                moderator
+                            @else
+                            @endif
                         </td>
                         <td class="px-6 py-4">
                             @if ($user->id === auth()->id())
--- resources/views/dashboard/user/show.blade.php
+++ resources/views/dashboard/user/show.blade.php
@@ -40,19 +40,7 @@
         </div>
         {{-- End of Email --}}
 
-        {{-- Role --}}
-        {{-- Role --}}
-<div>
-    <label for="role" class="block text-sm font-medium text-gray-700">Role</label>
-    <input
-        id="role"
-        type="text"
-        class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring focus:ring-opacity-50"
-        value="{{ $user->role == 1 ? 'Admin' : ($user->role == 2 ? 'User' : ($user->role == 3 ? 'Moderator' }}"
-        disabled
-    />
-</div>
-{{-- End of Role --}}
+
 
         {{-- Created At --}}
         <div>
